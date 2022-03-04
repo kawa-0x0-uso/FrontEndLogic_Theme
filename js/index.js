05465777071235a0d2ee4be23a382f81f7279ac2
kawa-0x0-uso <96374232+kawa-0x0-uso@users.noreply.github.com>
2022-03-04 15:46:54 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -14,13 +14,13 @@ const quizList = [
         type:'write',
         correct:'ロウリュ'
     },
-    {
-        // チェックボックス
-        question:'サウナが好きなキャラクターは？※複数回答※まだ表示だけ',
-        answer:['碧棺左馬刻','ミッキー','キティ','ムーミン'],
-        type:'check',
-        correct:['碧棺左馬刻','キティ']
-    }
+    // {
+    //     // チェックボックス
+    //     question:'サウナが好きなキャラクターは？※複数回答※まだ表示だけ',
+    //     answer:['碧棺左馬刻','ミッキー','キティ','ムーミン'],
+    //     type:'check',
+    //     correct:['碧棺左馬刻','キティ']
+    // }
 ];
 
 // 今何問目かをカウントする
@@ -34,21 +34,39 @@ function func_quiz_start(){
     let question_element = document.getElementById("quiz_question");
     let answer_element = document.getElementById("quiz_answer");
 
-    let answer_radio = '';
+    let radio_element = '';
+    let user_radio = '';
     let score = 0;
 
     if(current_number<quizList.length){
         // 問題文を表示
         question_element.textContent = quizList[current_number].question;
+
         // 回答のラジオボタン追加
         for(var i in quizList[current_number].answer){
-            answer_radio += '<input type="radio" name="answer_radio" value="'+quizList[current_number].answer[i]+'" id="radio'+i+'"><label for="radio'+i+'">'+quizList[current_number].answer[i]+'</label><br>';
+            radio_element += '<input type="radio" name="answer_radio" value="'+quizList[current_number].answer[i]+'" id="radio'+i+'"><label for="radio'+i+'">'+quizList[current_number].answer[i]+'</label><br>';
         }
-        answer_element.innerHTML = answer_radio;
+        answer_element.innerHTML = radio_element;
 
+        // checkedになっているラジオボタンの値を取得
+        let radio_check = document.getElementsByName('answer_radio');
+        for(let i = 0;i<radio_check.length;i++){
+            if(radio_check[i].checked){
+                user_radio = radio_check[i].value;
+            }
+        }
+
+        console.log('ユーザーの選択：'+user_radio);
+        // ラジオボタンの正誤判定
+        if(user_radio == quizList[current_number].correct){
+            score =+ 1 ;
+            console.log('得点：'+score);
+        }
+
+        // 次の問題を表示する
         current_number++;
     }else{
-        question_element.textContent = 'おわり';
+        current_number = 0;
     }
 }
 
@@ -88,4 +106,50 @@ function func_backgroundColor(){
         change_class.style.backgroundColor = colorList[count];
         count++;
     }
+}
+
+
+// ラジオボタン取得テスト
+const test_start = document.getElementById('test');
+test_start.addEventListener("click",func_test_start);
+let current_no = 0;
+
+const testList = [
+    {
+        // ラジオボタン
+        question:'サウナは何語？',
+        answer:['ロシア語','英語','フィンランド語','フランス語'],
+        type:'radio',
+        correct:'フィンランド語'
+    },
+    {
+        // 記述式
+        question:'赤く焼けた石の上に水をかけ蒸気を発生させるフィンランドの伝統的なサウナの方法を何という？※まだ表示だけ',
+        answer:'',
+        type:'write',
+        correct:'ロウリュ'
+    },
+];
+
+function func_test_start(){
+    console.log('func_test_startが動くかテスト');
+    let answer_element = document.getElementById("radio");
+    let radio_element = '';
+
+    let elems = document.getElementsByName("answer_radio");
+    let val = "";
+
+    // 回答のラジオボタン追加
+    // for(var i in testList[current_no].answer){
+    //     radio_element += '<input type="radio" name="answer_radio" value="'+quizList[current_number].answer[i]+'" id="radio'+i+'"><label for="radio'+i+'">'+quizList[current_number].answer[i]+'</label><br>';
+    // }
+    // answer_element.innerHTML = radio_element;
+
+    for (let i = 0; i < elems.length; i++) {
+        if (elems[i].checked){
+            val = elems[i].value;
+        }
+    } 
+
+    console.log(val);
 }
